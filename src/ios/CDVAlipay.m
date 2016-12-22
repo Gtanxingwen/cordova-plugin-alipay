ae4e17d2471c01594afefad50ae5375b2402cbb8
Gtanxingwen <1689947099@qq.com>
2016-12-22 17:00:03 +0800

    url

--- src/ios/CDVAlipay.m
+++ src/ios/CDVAlipay.m
@@ -12,7 +12,7 @@
 @implementation CDVAlipay
 // -(void)handleOpenURL:(NSNotification *)notification{
  - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString*, id> *)options{
-    NSURL* url = [notification object];
+//     NSURL* url = [notification object];
     //跳转支付宝钱包进行支付，需要将支付宝钱包的支付结果回传给SDK
     if (url!=nil && [url.host isEqualToString:@"safepay"]) {
         [[AlipaySDK defaultService]
